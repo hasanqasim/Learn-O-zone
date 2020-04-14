1dd824623b9b26df03b758de839c3d97760dd036
Laveeshka <lmah0006@student.monash.edu>
2020-04-14 19:12:24 +1000

    added code for Quiz

--- DataImport/index.js
+++ DataImport/index.js
@@ -1,8 +1,9 @@
 const admin = require('./node_modules/firebase-admin');
 const serviceAccount = require("./serviceAccountKey.json");
-const AQIdata = require("./AQI.json");
-
 const collectionKey = "AQIChart"; //name of the collection
+const quizCollectionKey = "Quizzes"; //name of the quiz collection
+const AQIdata = require("./AQI.json");
+const QuizData = require("./Quiz.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -13,7 +14,7 @@ admin.initializeApp({
   const settings = {timestampsInSnapshots: true};
   firestore.settings(settings);
 
-  //Checking if any data exists in data file and if it is object type
+  //Checking if any data exists in AQIdata file and if it is object type
   if (AQIdata && (typeof AQIdata === "object")) {
     Object.keys(AQIdata).forEach(docKey => { //Loop on data object
      firestore.collection(collectionKey).doc(docKey).set(AQIdata[docKey]) //Calling Firestore API to store each document, setting collection name and setting document name/key. 
@@ -25,3 +26,48 @@ admin.initializeApp({
     });
 
   }
+
+
+if (QuizData && (typeof QuizData === "object")) {
+  Object.keys(QuizData).forEach(docKey => {
+
+    const obj = QuizData[docKey];
+    console.log(obj);
+    const answersArray = []
+    Object.keys(obj).forEach(field => {
+
+      if (field === "Correct Answer"){
+        answersArray.push(obj[field]);
+      }
+      if (field === "Option1"){
+        answersArray.push(obj[field]);
+      }
+      if (field === "Option2"){
+        answersArray.push(obj[field]);
+      }
+      if (field === "Option3"){
+        answersArray.push(obj[field]);
+      }
+      
+
+    });
+
+    const newObj = {
+      QuizType: obj["Quiz Type"],
+      Question: obj["Question"],
+      Answers: answersArray,
+      Reason: obj["Reason"]
+    }
+
+    firestore.collection(quizCollectionKey).doc(docKey).set(newObj) //Calling Firestore API to store each document, setting collection name and setting document name/key. 
+    .then((res) => {
+       console.log("Document " + docKey + " successfully written!");
+   }).catch((error) => {
+      console.error("Error writing document: ", error);
+   });
+
+  });
+}
+    
+
+  
