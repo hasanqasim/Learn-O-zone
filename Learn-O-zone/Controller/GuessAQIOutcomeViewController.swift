ead574c96893770d646695ba0933a23f59c2f42d
Hasan Qasim <hasanqasim92@gmail.com>
2020-04-26 18:57:03 +1000

    add guess aqi controllers

---
+++ Learn-O-zone/Controller/GuessAQIOutcomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GuessAQIOutcomeViewController.swift
+//  Learn-O-zone
+//
+//  Created by Hasan Qasim on 26/4/20.
+//  Copyright © 2020 Hasan Qasim. All rights reserved.
+//
+
+import UIKit
+
+class GuessAQIOutcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
