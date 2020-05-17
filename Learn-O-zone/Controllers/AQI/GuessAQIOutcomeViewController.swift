83d5000954468437bfc0658fccf685818bb9b20a
Hasan Qasim <hasanqasim92@gmail.com>
2020-05-17 18:17:23 +1000

    fix navigation stack for aqi outcome screen

--- Learn-O-zone/Controllers/AQI/GuessAQIOutcomeViewController.swift
+++ Learn-O-zone/Controllers/AQI/GuessAQIOutcomeViewController.swift
@@ -18,75 +18,82 @@ class GuessAQIOutcomeViewController: UIViewController {
     var currentAQIValue: Double?
     var tappedButtonColor: String?
     var aqiObject: AirQualityResponse?
+    var aqiValue = Int()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         // this switch statement checks if the guess is correct or not then display relevant information accordingly
-        let aqiValue = Int(currentAQIValue!)
-        switch aqiValue {
-        case 0...50:
-            if (tappedButtonColor == "green") {
-            view.backgroundColor = UIColor(hexString: "ADD461")
-            } else{
-                view.backgroundColor = .gray
-                ozzyReactionIV.image = #imageLiteral(resourceName: "0ZZY-sad")
-                guessOutcome.text = "Wrong Guess"
-                congratMessage.text = "OOPS !"
-            }
-        case 51...100:
-            if (tappedButtonColor == "yellow") {
-                view.backgroundColor = UIColor(hexString: "F8D258")
-            } else {
-                view.backgroundColor = .gray
-                ozzyReactionIV.image = #imageLiteral(resourceName: "0ZZY-sad")
-                guessOutcome.text = "Wrong Guess"
-                congratMessage.text = "OOPS !"
-            }
-        case 101...150:
-            if (tappedButtonColor == "orange") {
-                view.backgroundColor = UIColor(hexString: "F39C5E")
-            } else {
-                view.backgroundColor = .gray
-                ozzyReactionIV.image = #imageLiteral(resourceName: "0ZZY-sad")
-                guessOutcome.text = "Wrong Guess"
-                congratMessage.text = "OOPS !"
-            }
-        case 151...200:
-            if (tappedButtonColor == "red") {
-                view.backgroundColor = UIColor(hexString: "F37674")
-            } else {
-                view.backgroundColor = .gray
-                ozzyReactionIV.image = #imageLiteral(resourceName: "0ZZY-sad")
-                guessOutcome.text = "Wrong Guess"
-                congratMessage.text = "OOPS !"
-            }
-        case 201...300:
-            if (tappedButtonColor == "purple") {
-                view.backgroundColor = UIColor(hexString: "A383BC")
-            } else {
-                view.backgroundColor = .gray
-                ozzyReactionIV.image = #imageLiteral(resourceName: "0ZZY-sad")
-                guessOutcome.text = "Wrong Guess"
-                congratMessage.text = "OOPS !"
-            }
-        case 301...500:
-            if (tappedButtonColor == "maroon") {
-                view.backgroundColor = UIColor(hexString: "945E74")
-            } else {
-                view.backgroundColor = .gray
-                ozzyReactionIV.image = #imageLiteral(resourceName: "0ZZY-sad")
-                guessOutcome.text = "Wrong Guess"
-                congratMessage.text = "OOPS !"
-            }
-        default:
-            view.backgroundColor = .gray
-        }
-        
-        //this fills the username label with the username of the logged in user
+        self.navigationItem.setHidesBackButton(true, animated: true);
+        aqiValue = Int(currentAQIValue!)
+        switchStatement()
         usernameLabel.text = CurrentUser.getCurrentUser().username
     }
-    
+        
+        //this fills the username label with the username of the logged in user
+    func switchStatement() {
+        switch aqiValue {
+            case 0...50:
+                if (tappedButtonColor == "green") {
+                view.backgroundColor = UIColor(hexString: "ADD461")
+                } else{
+                    view.backgroundColor = .gray
+                    ozzyReactionIV.image = #imageLiteral(resourceName: "0ZZY-sad")
+                    guessOutcome.text = "Wrong Guess"
+                    congratMessage.text = "OOPS !"
+                }
+            case 51...100:
+                if (tappedButtonColor == "yellow") {
+                    view.backgroundColor = UIColor(hexString: "F8D258")
+                } else {
+                    view.backgroundColor = .gray
+                    ozzyReactionIV.image = #imageLiteral(resourceName: "0ZZY-sad")
+                    guessOutcome.text = "Wrong Guess"
+                    congratMessage.text = "OOPS !"
+                }
+            case 101...150:
+                if (tappedButtonColor == "orange") {
+                    view.backgroundColor = UIColor(hexString: "F39C5E")
+                } else {
+                    view.backgroundColor = .gray
+                    ozzyReactionIV.image = #imageLiteral(resourceName: "0ZZY-sad")
+                    guessOutcome.text = "Wrong Guess"
+                    congratMessage.text = "OOPS !"
+                }
+            case 151...200:
+                if (tappedButtonColor == "red") {
+                    view.backgroundColor = UIColor(hexString: "F37674")
+                } else {
+                    view.backgroundColor = .gray
+                    ozzyReactionIV.image = #imageLiteral(resourceName: "0ZZY-sad")
+                    guessOutcome.text = "Wrong Guess"
+                    congratMessage.text = "OOPS !"
+                }
+            case 201...300:
+                if (tappedButtonColor == "purple") {
+                    view.backgroundColor = UIColor(hexString: "A383BC")
+                } else {
+                    view.backgroundColor = .gray
+                    ozzyReactionIV.image = #imageLiteral(resourceName: "0ZZY-sad")
+                    guessOutcome.text = "Wrong Guess"
+                    congratMessage.text = "OOPS !"
+                }
+            case 301...500:
+                if (tappedButtonColor == "maroon") {
+                    view.backgroundColor = UIColor(hexString: "945E74")
+                } else {
+                    view.backgroundColor = .gray
+                    ozzyReactionIV.image = #imageLiteral(resourceName: "0ZZY-sad")
+                    guessOutcome.text = "Wrong Guess"
+                    congratMessage.text = "OOPS !"
+                }
+            default:
+                view.backgroundColor = .gray
+        
+        }
+    }
+        
 
     /*
     // MARK: - Navigation
@@ -98,4 +105,7 @@ class GuessAQIOutcomeViewController: UIViewController {
     }
     */
 
+    @IBAction func backToAQIScreenButtonTapped(_ sender: Any) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
 }
