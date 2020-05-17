6ea1c2a1084b9d9c760d1ce406d58a84cf5c3ff7
Laveeshka <lmah0006@student.monash.edu>
2020-05-17 11:12:28 +1000

    fixed pollutant concentration unit, added max concentrations, textView for AQI description

--- Learn-O-zone/Controllers/AQI/PollutantInformationViewController.swift
+++ Learn-O-zone/Controllers/AQI/PollutantInformationViewController.swift
@@ -11,8 +11,9 @@ import UIKit
 class PollutantInformationViewController: UIViewController {
     //let pollutants = [pm25Obj, pm10Obj, so2Obj, o3Obj, no2Obj, coObj]
     
+    @IBOutlet weak var healthRecommendation: UITextView!
     @IBOutlet weak var healthRecommendationView: UIView!
-    @IBOutlet weak var healthRecommendation: UILabel!
+ 
     @IBOutlet weak var aqiValue: UILabel!
     @IBOutlet weak var o3Value: UILabel!
     @IBOutlet weak var co2Value: UILabel!
@@ -25,16 +26,23 @@ class PollutantInformationViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        healthRecommendationView.layer.cornerRadius = 10.0
         
+        //rounding the corner edges of the heath recommendation view
+        healthRecommendationView.layer.cornerRadius = CGFloat(Helper.app.smallButtonCornerRadius)
+        
+        //assigning the pollutant concentrations to the relevant labels
+        //rounding the concentration value for o3, no2, so2 and co to 3 decimal places
+        //converting ppb values to ppm values
         aqiValue.text = "\(aqiObject!.aqiValue)"
-        o3Value.text = "\(aqiObject!.pollutants[3].value)"
-        co2Value.text = "\(aqiObject!.pollutants[5].value)"
+        o3Value.text = String(format: "%.3f", aqiObject!.pollutants[3].value/1000)
+        co2Value.text = String(format: "%.3f", aqiObject!.pollutants[3].value/1000)
+        so2Value.text = String(format: "%.3f", aqiObject!.pollutants[3].value/1000)
+        no2Value.text = String(format: "%.3f", aqiObject!.pollutants[3].value/1000)
         pm25Value.text = "\(aqiObject!.pollutants[0].value)"
         pm10Value.text = "\(aqiObject!.pollutants[1].value)"
-        so2Value.text = "\(aqiObject!.pollutants[2].value)"
-        no2Value.text = "\(aqiObject!.pollutants[4].value)"
         
+        
+        //extracting the last line, the most relevant health recommendation for kids from the air quality reponse object and assigning it to the TextView
         let lines = aqiObject!.healthRecommendation.components(separatedBy: ". ")
         healthRecommendation.text = "\(lines.last ?? "Please be safe outside")"
         
